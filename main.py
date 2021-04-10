a3f90b68a1f42121443f76133bddcad02c57d3fa
Hemanth Kumar M <30370376+Hemanthkumar2112@users.noreply.github.com>
2021-04-10 02:42:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-
+data = [1,2,3,4]
 app=Flask(__name__)
 
 @app.route('/' , methods = ['GET','POST'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html' , data = data)
 
 
 
